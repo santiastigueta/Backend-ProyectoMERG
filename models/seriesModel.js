caed1833da22265fb47a57827833a8c0009e2b2f
santi <santiasti0906@gmail.com>
2022-08-19 12:07:31 -0300

    primer commit

---
+++ models/seriesModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+//import { Schema } from "mongoose";
+
+const serieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    author: {
+        type: String
+    },
+    rating: {
+        type: String
+    },
+    releaseDate: {
+        type: String
+    }
+
+});
+
+const Series = mongoose.model('Series', serieSchema);
+
+export default Series;
